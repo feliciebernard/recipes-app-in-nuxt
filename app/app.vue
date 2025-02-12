d63d4187d114e3c0bb298c678ec2c3ea7391da87
Felicie <felicie@gabie.app>
2025-02-12 19:00:15 +0100

    first commit

---
+++ app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-lg text-blue-400">Hello</h1>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,38 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  experimental: {},
+
+  unhead: {},
+
+  devtools: { enabled: true },
+  compatibilityDate: '2025-02-11',
+
+  typescript: {
+    typeCheck: true,
+  },
+
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/image',
+    '@nuxt/icon',
+    [
+      '@nuxtjs/google-fonts',
+      {
+        families: {
+          Roboto: true,
+          Inter: [400, 700],
+          'Josefin+Sans': true,
+          Lato: [100, 300],
+          Raleway: {
+            wght: [100, 400],
+            ital: [100],
+          },
+        },
+      },
+    ],
+  ],
+})
